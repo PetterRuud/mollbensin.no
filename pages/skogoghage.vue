d7bd7339a3e9f10e9dd86150a0b8f71d6491a980
Petter Ruud <petterruud@gmail.com>
2019-09-09 22:12:07 +0200

    bakgrunnsbilder

--- pages/skogoghage.vue
+++ pages/skogoghage.vue
@@ -1,135 +1,139 @@
 <template>
-  <article class="container gutter">
-    <h1>Skog og hage</h1>
-    <p>
-      Vi over 50 års fartstid som fagforhandler. Dette nyter du som kunde godt
-      av, enten du er proff eller hobbybruker! Hos oss får du blant
-      gressklippere, robotklippere, traktorklippere, motorsager, gresstrimmere
-      og andre skog- og hageprodukter.
-    </p>
-
-    <p>
-      I butikken finner du også et stort utvalg verneutstyr, hansker,
-      miljøbensin, sagkjeder og annet tilbehør. Her finner du også de fleste
-      modeller utstilt. Kom innom en tur, så hjelper vi med å finne riktig
-      maskin for deg.
-    </p>
-
-    <aside>
+  <div>
+    <article class="container gutter">
+      <h1>Skog og hage</h1>
       <p>
-        Du kan også lese mer om og se priser på et utvalg av våre bestselgende
-        maskiner på
-        <a href="http://www.finn.no/butikk/mollb.html"
-          >vår butikkside på Finn.no</a
-        >.
+        Vi over 50 års fartstid som fagforhandler. Dette nyter du som kunde godt
+        av, enten du er proff eller hobbybruker! Hos oss får du blant
+        gressklippere, robotklippere, traktorklippere, motorsager, gresstrimmere
+        og andre skog- og hageprodukter.
       </p>
-    </aside>
 
-    <div class="grid">
-      <div class="box">
-        <div class="box-inner">
-          <img src="~/static/H350-0271.jpg" />
-          <div class="box-content">
-            <h2>Robotklippere</h2>
-            <p>
-              Med en robotgressklipper kan du få en frodig, grønn plen uten å
-              måtte bruke tid på gressklipping. Dette er batteridrevne
-              gressklippere som slår gresset på egenhånd. Vi leverer
-              robotklippere fra Husqvarna og Honda.
-            </p>
+      <p>
+        I butikken finner du også et stort utvalg verneutstyr, hansker,
+        miljøbensin, sagkjeder og annet tilbehør. Her finner du også de fleste
+        modeller utstilt. Kom innom en tur, så hjelper vi med å finne riktig
+        maskin for deg.
+      </p>
+
+      <aside>
+        <p>
+          Du kan også lese mer om og se priser på et utvalg av våre bestselgende
+          maskiner på
+          <a href="http://www.finn.no/butikk/mollb.html"
+            >vår butikkside på Finn.no</a
+          >.
+        </p>
+      </aside>
+
+      <div class="grid">
+        <div class="box">
+          <div class="box-inner">
+            <img src="~/static/H350-0271.jpg" />
+            <div class="box-content">
+              <h2>Robotklippere</h2>
+              <p>
+                Med en robotgressklipper kan du få en frodig, grønn plen uten å
+                måtte bruke tid på gressklipping. Dette er batteridrevne
+                gressklippere som slår gresset på egenhånd. Vi leverer
+                robotklippere fra Husqvarna og Honda.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="box">
+          <div class="box-inner">
+            <img src="~/static/gressklipper-jo.jpg" />
+            <div class="box-content">
+              <h2>Gressklippere</h2>
+              <p>
+                En gressklipper er ikke bare en gressklipper. Vi har et stort
+                utvalg klippere med funksjonalitet som trinnløs fremdrift,
+                bioklipp, knivstopp og elektrisk start, for å nevne noe. Kom
+                innom en tur, så hjelper vi deg å finne en modell som passer
+                dine behov. Vi har klippere fra Honda, Jonsered og Klippo.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="box">
+          <div class="box-inner">
+            <img src="~/static/hq-motorsag.jpg" />
+            <div class="box-content">
+              <h2>Motorsager</h2>
+              <p>
+                Våre sager har den mest avanserte motorsagteknologien, uten at
+                det går på bekostning av brukervennligheten. Vi har sager fra
+                Jonsered og Husqvarna til fritidsbruk, vedkapping og for
+                skogsarbeideren.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="box">
+          <div class="box-inner">
+            <img src="~/static/fr-jo.jpg" />
+            <div class="box-content">
+              <h2>Traktorklippere og frontridere</h2>
+              <p>
+                En sitteklipper er det beste valget for store hager! Vi har både
+                traktorklippere og frontridere fra Jonsered og Husqvarna.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="box">
+          <div class="box-inner">
+            <img src="~/static/trimmer-jo.jpg" />
+            <div class="box-content">
+              <h2>Trimmere og ryddere</h2>
+              <p>
+                Gresstrimmere og kantklippere kommer i mange varianter, fra
+                enkle gresstrimmere til avanserte ryddesager. Vi hjelper å finne
+                en maskin som passer perfekt til dine behov. Vi har trimmere fra
+                Honda, Jonsered og Husqvarna.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="box">
+          <div class="box-inner">
+            <img src="~/static/EU20i.jpg" />
+            <div class="box-content">
+              <h2>Aggregater</h2>
+              <p>
+                Hondas aggregater er konstruert for å være en pålitelige kilde
+                til strøm av høy kvalitet. Utviklet for å håndtere de
+                vanskeligste omgivelser og det mest krevende profesjonelle
+                utstyret, løser de oppgavene med betryggende letthet og
+                effektivitet.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="box">
+          <div class="box-inner">
+            <img src="~/static/snofres-ho.jpg" />
+            <div class="box-content">
+              <h2>Snøfresere</h2>
+              <p>
+                Vi tar snøfresing på alvor! Hos oss får du kvalitetsfresere fra
+                Honda og Jonsered, som holder år etter år. Modellene kommer i
+                ulike størrelser, med hjul eller belte. Du kan være trygg på at
+                du alltid vil få tak i slitedeler hos oss.
+              </p>
+            </div>
           </div>
         </div>
       </div>
-
-      <div class="box">
-        <div class="box-inner">
-          <img src="~/static/gressklipper-jo.jpg" />
-          <div class="box-content">
-            <h2>Gressklippere</h2>
-            <p>
-              En gressklipper er ikke bare en gressklipper. Vi har et stort
-              utvalg klippere med funksjonalitet som trinnløs fremdrift,
-              bioklipp, knivstopp og elektrisk start, for å nevne noe. Kom innom
-              en tur, så hjelper vi deg å finne en modell som passer dine behov.
-              Vi har klippere fra Honda, Jonsered og Klippo.
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="box">
-        <div class="box-inner">
-          <img src="~/static/hq-motorsag.jpg" />
-          <div class="box-content">
-            <h2>Motorsager</h2>
-            <p>
-              Våre sager har den mest avanserte motorsagteknologien, uten at det
-              går på bekostning av brukervennligheten. Vi har sager fra Jonsered
-              og Husqvarna til fritidsbruk, vedkapping og for skogsarbeideren.
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="box">
-        <div class="box-inner">
-          <img src="~/static/fr-jo.jpg" />
-          <div class="box-content">
-            <h2>Traktorklippere og frontridere</h2>
-            <p>
-              En sitteklipper er det beste valget for store hager! Vi har både
-              traktorklippere og frontridere fra Jonsered og Husqvarna.
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="box">
-        <div class="box-inner">
-          <img src="~/static/trimmer-jo.jpg" />
-          <div class="box-content">
-            <h2>Trimmere og ryddere</h2>
-            <p>
-              Gresstrimmere og kantklippere kommer i mange varianter, fra enkle
-              gresstrimmere til avanserte ryddesager. Vi hjelper å finne en
-              maskin som passer perfekt til dine behov. Vi har trimmere fra
-              Honda, Jonsered og Husqvarna.
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="box">
-        <div class="box-inner">
-          <img src="~/static/EU20i.jpg" />
-          <div class="box-content">
-            <h2>Aggregater</h2>
-            <p>
-              Hondas aggregater er konstruert for å være en pålitelige kilde til
-              strøm av høy kvalitet. Utviklet for å håndtere de vanskeligste
-              omgivelser og det mest krevende profesjonelle utstyret, løser de
-              oppgavene med betryggende letthet og effektivitet.
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="box">
-        <div class="box-inner">
-          <img src="~/static/snofres-ho.jpg" />
-          <div class="box-content">
-            <h2>Snøfresere</h2>
-            <p>
-              Vi tar snøfresing på alvor! Hos oss får du kvalitetsfresere fra
-              Honda og Jonsered, som holder år etter år. Modellene kommer i
-              ulike størrelser, med hjul eller belte. Du kan være trygg på at du
-              alltid vil få tak i slitedeler hos oss.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </article>
+    </article>
+  </div>
 </template>
 
 <style lang="scss" scoped>
--- pages/verksted.vue
+++ pages/verksted.vue
@@ -1,21 +1,31 @@
 <template>
-  <article class="container gutter">
-    <h1>Verksted</h1>
-    <p>
-      Vi tilbyr service, vedlikehold og reperasjoner av motorsager,
-      gressklippere, snøfresere og andre maskiner fra Honda, Jonsered, Husqvarna
-      og de fleste andre merker.
-    </p>
-    <p>
-      Verkstedet på Møll Bensinstasjon er åpent fra 08.00 til 16.00, mandag til
-      fredag. Det samme gjelder vårt reservedelslager. Enklere deler som kniver,
-      plugger, kjeder og lignende har vi tilgjengelig i stasjonens åpningstid.
-    </p>
-    <p>
-      <strong
-        >Ring oss på tlf. 382 67 144 for en uforpliktende prat om ditt
-        servicebehov.</strong
-      >
-    </p>
-  </article>
+  <div>
+    <div class="featureimage" />
+    <article class="container gutter">
+      <h1>Verksted</h1>
+      <p>
+        Vi tilbyr service, vedlikehold og reperasjoner av motorsager,
+        gressklippere, snøfresere og andre maskiner fra Honda, Jonsered,
+        Husqvarna og de fleste andre merker.
+      </p>
+      <p>
+        Verkstedet på Møll Bensinstasjon er åpent fra 08.00 til 16.00, mandag
+        til fredag. Det samme gjelder vårt reservedelslager. Enklere deler som
+        kniver, plugger, kjeder og lignende har vi tilgjengelig i stasjonens
+        åpningstid.
+      </p>
+      <p>
+        <strong
+          >Ring oss på tlf. 382 67 144 for en uforpliktende prat om ditt
+          servicebehov.</strong
+        >
+      </p>
+    </article>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.featureimage {
+  background-image: url('~static/Serviceverksted.jpg');
+}
+</style>
